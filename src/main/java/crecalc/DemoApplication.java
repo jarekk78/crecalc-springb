ecad4df29c7b5a7a0ad0af1393d4707f186f079b
Jaroslaw Kacerka <kacerkaj@gmail.com>
2017-08-19 20:31:42 +0200

    cleanup

--- src/main/java/crecalc/DemoApplication.java
+++ src/main/java/crecalc/DemoApplication.java
@@ -1,7 +1,10 @@
-package com.example.crecalcspringb;
+package crecalc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @SpringBootApplication
